af9211eb1156b4f96b69dace6e0978a04aa6eb1f
Maxim Repin <ragemax99@yahoo.com>
2022-03-04 12:57:35 +0300

    Styles add

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+// This widget is the root of your application
   @override
   Widget build(BuildContext context) {
+    const borderStyle= OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(36)),
+      borderSide: BorderSide(
+        color: const Color(0xFFbbbbbb), width: 2));
+
+
     return MaterialApp(
       home: Scaffold(
           body: Column(children: [
-            Padding(
+            const Padding(
                 padding: EdgeInsets.only(top: 140.0),
                 child: Center(child: SizedBox(width: 104, height: 80, child: Placeholder(),))),
-            Padding(
+            const Padding(
                 padding: EdgeInsets.only(top: 18.0),
-                child: Text('Введите логин в виде 10 цифр номера телефона')),
+                child: Text('Введите логин в виде 10 цифр номера телефона',
+                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),)),
             Padding(
                 padding: EdgeInsets.fromLTRB(38.0, 20.0, 38.0, 20.0),
                 child: TextField(
@@ -26,6 +32,8 @@ class MyApp extends StatelessWidget {
                     filled: true,
                     fillColor: Color(0xFFeceff1),
                     labelText: 'Телефон',
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
                   ),
                 )),
             Padding(
@@ -36,6 +44,8 @@ class MyApp extends StatelessWidget {
                     filled: true,
                     fillColor: Color(0xFFeceff1),
                     labelText: 'Пароль',
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
                   ),
                 )),
             Padding(
@@ -44,13 +54,33 @@ class MyApp extends StatelessWidget {
                   child: ElevatedButton(
                       onPressed: () {},
                       child: Text('Войти'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF0079D0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(36.0)
+                        )
+                      ),
 
                   ),
                 )),
-            InkWell(child: Text('Регистрация'), onTap: () {}),
+            InkWell(
+                child: Text('Регистрация',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xFF0079D0),
+                    fontWeight: FontWeight.bold,
+                ),),
+                onTap: () {}),
             Padding(
                 padding: EdgeInsets.only(top: 19.0),
-                child: InkWell(child: Text('Забыли пароль?'), onTap: () {})),
+                child: InkWell(
+                    child: Text('Забыли пароль?',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xFF0079D0),
+                        fontWeight: FontWeight.bold
+                    ),),
+                    onTap: () {})),
           ],)
       ),
     );

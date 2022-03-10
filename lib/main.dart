b317de82063ef81c6276f60c70aa28c639a7d63f
Maxim Repin <ragemax99@yahoo.com>
2022-03-10 23:25:21 +0300

    2.5 ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:auth_page/screens/app_bar_screen.dart';
+import 'package:auth_page/screens/listview_screen.dart';
 import 'package:auth_page/screens/navigation_demo.dart';
 import 'package:auth_page/screens/navigation_drawer_screen.dart';
 import 'package:auth_page/screens/switches-demo-two.dart';
@@ -8,5 +9,5 @@ import 'package:auth_page/screens/registration.dart';
 import 'package:auth_page/screens/switches-demo-screen.dart';
 
 void main() {
-  runApp(const SwitchesDemoTwo());
+  runApp(const ListViewScreen());
 }

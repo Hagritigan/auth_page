2995a0228ebd26f214c52fe653ece6c640695cf1
Maxim Repin <ragemax99@yahoo.com>
2022-03-11 16:20:32 +0300

    3.2 ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:auth_page/screens/app_bar_screen.dart';
+import 'package:auth_page/screens/files_demo_screen.dart';
 import 'package:auth_page/screens/listview_screen.dart';
 import 'package:auth_page/screens/navigation_demo.dart';
 import 'package:auth_page/screens/navigation_drawer_screen.dart';
+import 'package:auth_page/screens/networking_screen.dart';
+import 'package:auth_page/screens/shared_pref_screen.dart';
 import 'package:auth_page/screens/switches-demo-two.dart';
 import 'package:flutter/material.dart';
 import 'package:auth_page/screens/auth.dart';
@@ -9,5 +12,5 @@ import 'package:auth_page/screens/registration.dart';
 import 'package:auth_page/screens/switches-demo-screen.dart';
 
 void main() {
-  runApp(const ListViewScreen());
+  runApp(const NetworkingScreen());
 }
